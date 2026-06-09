e0655bf432f31310a28c278a2541d3d350542690
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-06-09 18:28:03 +0530

    Git practice

--- git-github/demo-project/Demo.java
+++ git-github/demo-project/Demo.java
@@ -1,5 +1,11 @@
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Deepak");
+        display();
+        display();
+    }
+
+    public static void display() {
+        System.out.println("Name : Deepak Dey");
     }
 }
